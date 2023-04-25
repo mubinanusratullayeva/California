7c72b8effc79d857d75f3ff15f085b76d15b255b
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-04-25 09:22:37 -0700

    Looking for anything else?

---
+++ JavaScript/app.js
@@ -0,0 +1,9 @@
+// const inputSearch = document.querySelector('.form__validation-insea'),
+//        btnItem = document.querySelectorAll('.buttons-team--item');
+
+
+//        inputSearch.addEventListener('click',  () =>{
+//         if (inputSearch.value == btnItem.value) {
+//             btnItem.classList.add('styleBtn');
+//      }
+//        })
